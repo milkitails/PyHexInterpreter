91b438fd1cf858ebeec5390b9c256ce227a25f4c
milkitails <llypayutu@gmail.com>
2025-06-25 14:23:34 +0300

    Add files via upload

--- PyHexInterpreter.py
+++ PyHexInterpreter.py
@@ -7,10 +7,10 @@ parser = argparse.ArgumentParser(description='PyHexInterpreter')
 parser.add_argument('file', type=str, help='.bin file')
 args = parser.parse_args()
 
-with open(args.file, "r") as file:
+with open(args.file, "r", encoding="utf-8") as file:
 	path = os.environ.get("TEMP") + f"\\{random.randint(0, 872870)}.py"
-	with open(path, "a") as sec_file:
+	with open(path, "a", encoding="utf-8") as sec_file:
 		sec_file.write(binascii.unhexlify(file.read()).decode())
 
-os.system(f"{os.environ.get("APPDATA")}\\python_310\\python.exe {path}")
+os.system(f"{os.environ.get('APPDATA')}\\python_310\\python.exe {path}")
 os.system(f"del {path}")
